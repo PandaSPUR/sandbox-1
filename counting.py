5a4c24063aa8409ebc6bd768e1e994b1b493c866
TheBananaStand <dominic.spinosa@gmail.com>
2013-09-17 12:50:20 -0400

    counting Python program altered

--- counting.py
+++ counting.py
@@ -1,6 +1,16 @@
-# This program counts down from 10 to 1
+#///////////////////////////////////////////////////////////////
+#
+#	Dominic Spinosa
+#	Application Security
+#	Assignment 1 - Turing Complete Sandbox
+#	Test Case 1
+#	9/12/13
+#
+#	This program counts down from 10 to 1
+#
+#///////////////////////////////////////////////////////////////
 
-for(i = 10; i > 0; i--):
-	print(i)
+for x in range(10, 0, -1):
+	print(x)
 
 
